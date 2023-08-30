637c0a7a2d14896f92f6384ca2e22894d786b585
Adityamishra9719 <adityamishra971916@gmail.com>
2023-08-30 16:39:40 +0530

    Aditya

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+# !/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build 
+
+# navigate into the build output directory
+cd dist
+
+# place .nojekyll to bypass jekyll processing
+echo > .nojekyll
+
+
+
+git init --config
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+
+git push -f git@github.com:adityakmrmishra/3D-Portfolio.git main:gh-pages
+
+
+cd-
